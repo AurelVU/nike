0fd4b7b863c32c54279f2b91c5aa3edae72f3ac6
Ушаков Владимир <eau120676@gmail.com>
2023-11-19 21:16:18 +0300

    Initial Commit

---
+++ nike/nikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  nikeApp.swift
+//  nike
+//
+//  Created by Владимир Ушаков on 19.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct nikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
